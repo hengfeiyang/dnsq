4104d19870ac0a36f07adb63a228d562c6227f9c
coldstar <safeie@163.com>
2017-03-01 19:56:45 +0800

    更新配置依赖

--- config.go
+++ config.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 
-	"git.vodjk.com/totoro/totoro/common/util"
 	"gopkg.in/yaml.v2"
 )
 
@@ -24,10 +25,20 @@ type Configer struct {
 // NewConfig 读取配置文件，实例化配置并返回
 func NewConfig(filename string) (*Configer, error) {
 	c := new(Configer)
-	data, err := util.ReadFile(filename)
+	data, err := ReadFile(filename)
 	if err != nil {
 		return nil, fmt.Errorf("config.parse error: %s", err.Error())
 	}
 	err = yaml.Unmarshal(data, c)
 	return c, nil
+}
+
+// ReadFile read file content
+func ReadFile(file string) ([]byte, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return ioutil.ReadAll(f)
 }
